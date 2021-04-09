66a2568b5f5cd311e5becf86d8012af5ef6d370a
kb1981 <79020160+kb1981@users.noreply.github.com>
2021-04-09 16:39:42 -0700

    Add files via upload
    UW 310 Python Programming
    - Python Pushups Lesson 1

---
+++ break_me.py
@@ -0,0 +1,23 @@
+# Date: 2021-4-9
+# Created by:  Kimberly Brock
+# Purpose: Class Exercise, Python Push-Ups
+#   Each function, when called, should cause an exception to happen
+#   Each function should result in one of the four most common exceptions you’ll find.
+#   for review: - NameError - TypeError - SyntaxError - AttributeError
+
+# AttributeError
+num = 2
+num.upper()
+
+# Syntax Error
+42.upper()
+
+# TypeError
+num1 = 1
+num2 = "2"
+ans = num1 + num2
+
+# NameError
+print ("hello world!")
+pirnt ("hello world")
+
